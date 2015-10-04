20cb4aa12d5ad0344af9586c33d2336469be281b
Deeee <liudii@me.com>
2015-10-04 17:59:52 -0400

    Rename users.js to recording.js

--- routes/recording.js
+++ routes/recording.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* Get listing recordings */
+router.get('/list', function(req,res, next) {
+  // res.send(apple = appl, orange = org, charles, crls);
+  res.json([{a:1222}, {a:233333}, {a:34444}]);
+});
+
+
 module.exports = router;
