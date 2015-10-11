9a3080cccc11217611374160e7f455e891f6d1be
Deeee <liudii@me.com>
2015-10-11 16:40:29 -0400

    method for sending message to database

--- app.js
+++ app.js
@@ -4,23 +4,38 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var Parse = require('parse/node').Parse;
 var routes = require('./routes/index');
 var recordings = require('./routes/recording');
 
 var app = express();
 
 // // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // // uncomment after placing your favicon in /public
-// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+Parse.initialize("8Nx1MZhNZzI6jw1SM73isCHpmGGIPBvx0OQTJJl3","jU9dbSvBPVQLHD9saDx4PU7FNvqUkxZLCYFgLLpq");
+//
+var RecordingObject = Parse.Object.extend("RecordingObject");
+var recordingObject = new RecordingObject();
+console.log("here !");
+recordingObject.save({foo: "bar"}, {
+  success: function(Object) {
+      console.log("success!!!");
+  },
+  error : function(obj, error) {
+    console.log(error);
+  }
+});
+
 
 app.use('/', routes);
 app.use('/recording', recordings);
--- routes/recording.js
+++ routes/recording.js
@@ -1,16 +1,59 @@
 var express = require('express');
 var router = express.Router();
+var Parse = require('parse/node').Parse;
+Parse.initialize("8Nx1MZhNZzI6jw1SM73isCHpmGGIPBvx0OQTJJl3","jU9dbSvBPVQLHD9saDx4PU7FNvqUkxZLCYFgLLpq");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+
+
 /* Get listing recordings */
 router.get('/list', function(req,res, next) {
   // res.send(apple = appl, orange = org, charles, crls);
+  var Recording = Parse.Object.extend("RecordingObject");
+  var query = new Parse.Query(Recording);
+  query.forEach({
+    success: function(results) {
+      console.log("Successfully retrieved " + results.length);
+      // Do something with the returned Parse.Object values
+      for (var i = 0; i < results.length; i++) {
+        var object = results[i];
+        console.log(object.id + ' - ' + object.get('column'));
+      }
+      status.message("Successfully retrieved " + results.length );
+    },
+    error: function(error) {
+      status.error("Error: " + error.code + " " + error.message);
+    }
+  });
+
   res.json([{a:1222}, {a:233333}, {a:34444}]);
 });
+
+router.get('/send', function(req,res, next) {
+  //
+  var RecordingObject = Parse.Object.extend("RecordingObject");
+  var recordingObject = new RecordingObject();
+  console.log("here !");
+  recordingObject.save({foo: "bar"}, {
+    success: function(Object) {
+        console.log("success!!!");
+    },
+    error : function(obj, error) {
+      console.log(error);
+    }
+  });
+  res.json([{a:1222}, {a:233333}, {a:34444}]);
+}
+
+/* method handling post request*/
+// router.get('/post', function(req,res, next,post){
+//
+// });
 
 
 module.exports = router;
